1f3a7449fd823d768a73f24df400662fe7b388c8
Shadowfacts <me@shadowfacts.net>
2015-06-04 21:50:12 -0400

    Reorganize AbstractIRCConnectionManagerTest

--- src/test/java/com/github/blamevic/irc/AbstractIRCConnectionManagerTest.java
+++ src/test/java/com/github/blamevic/irc/AbstractIRCConnectionManagerTest.java
@@ -1,58 +1,67 @@
 package com.github.blamevic.irc;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
+/**
+ * Abstract class that can be extended/used for all {@link com.github.blamevic.irc.IRCConnectionManager}
+ *
+ * @author rx14
+ * @author shadowfacts
+ */
 public abstract class AbstractIRCConnectionManagerTest {
 
     /**
-     * @return a new IIRCConnectionManager
+     * @return The {@link com.github.blamevic.irc.IRCConnectionManager} to be used for this subclass of {@link com.github.blamevic.irc.AbstractIRCConnectionManagerTest}
      */
     public abstract IRCConnectionManager getManager(IRCClient client);
 
+	IRCClient client;
     IRCConnectionManager manager;
 
     @Before
-    public void setUp() throws Exception {
-        //manager = getManager();
-    }
+    public void setUp() throws IOException {
+		client = new IRCClient("localhost", 6667, "shadowfacts", "shadowfacts", true);
+		client.connect();
+		manager = getManager(client);
+	}
 
-    @Test
-    public void testGetChannels() throws Exception {
-        manager.joinChannel("#test");
-        manager.joinChannel("#foo");
+	@After
+	public void exit() {
+		manager.channels().forEach(manager::leaveChannel);
+	}
 
-        assertTrue("can read back channels", manager.channels().contains("#test"));
+	@Test
+	public void testGetChannels() {
+		manager.joinChannel("#test");
+		assertTrue("can get channel back", manager.channels().contains("#test"));
+	}
 
-        manager.leaveChannel("#test");
+	@Test
+	public void testLeaveChannel() {
+		manager.joinChannel("#test");
+		manager.leaveChannel("#test");
+		assertFalse("can leave channel", manager.channels().contains("#test"));
+	}
 
-        assertFalse("can remove channels", manager.channels().contains("#test"));
+	@Test
+	public void testLeaveChannelWithReason() {
+		manager.joinChannel("#test");
+		manager.leaveChannel("#test", "RX14-chibi told me to");
+		assertFalse("can leave channel with reason", manager.channels().contains("#test"));
+	}
 
-        manager.leaveChannel("#foo", "I'm leaving because of #bar");
-
-        assertFalse("can remove channels with reason", manager.channels().contains("#foo"));
-        assertTrue("after the test, i have left all channels", manager.channels().isEmpty());
-    }
-
-    @Test
-    public void testJoinChannel() throws Exception {
-
-    }
-
-    @Test
-    public void testLeaveChannel() throws Exception {
-
-    }
-
-    @Test
-    public void testLeaveChannel1() throws Exception {
-
-    }
-
-    @Test
-    public void testGetEnumeratorForChannel() throws Exception {
-
-    }
+	@Test
+	public void testLeaveAllChannels() {
+		manager.joinChannel("#test");
+		manager.joinChannel("#foo");
+		manager.leaveChannel("#test");
+		manager.leaveChannel("#foo");
+		assertTrue("can leave all channels", manager.channels().isEmpty());
+	}
 }
